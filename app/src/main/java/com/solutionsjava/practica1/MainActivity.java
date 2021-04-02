e0125d5daa1e97bee3b16d05f810fd14919bc914
roerES <erodriguezven@gmail.com>
2021-04-01 21:22:00 -0400

    Initial commit

---
+++ app/src/main/java/com/solutionsjava/practica1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.solutionsjava.practica1;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
